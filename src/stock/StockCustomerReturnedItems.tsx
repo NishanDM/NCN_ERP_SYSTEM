b897034eeedd842427f142fb15e51825ff8801a6
HashiniPrabuddhika <prabuddhikarph@gmail.com>
2026-08-11 18:04:07 +0530

    feat(stock): add CustomerReturnedItemsManagement page component

---
+++ src/stock/StockCustomerReturnedItems.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function StockCustomerReturnedItems() {
+  return (
+    <div>
+      <h1>Customer Returned Items Management is Here</h1>
+    </div>
+  )
+}
+
+export default StockCustomerReturnedItems
