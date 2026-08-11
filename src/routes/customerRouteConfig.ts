2051958bdd49e254badb1519186aecb818d55a5b
NishanDM <nishandm97@gmail.com>
2026-08-12 00:04:52 +0530

    Lashini Updates Error Correction

--- src/routes/customerRouteConfig.ts
+++
@@ -1,8 +0,0 @@
-import { CustomerUserPage } from '../pages/customer_user/CustomerUserPage';
-
-export const customerRouteConfig = [
-  {
-    path: '/customer-portal',
-    component: CustomerUserPage,
-  },
-];
