bdd1b34d9c3cf6f721b06fa97bb243a00e7de611
Lashini Vithanage <lashinivithanage123@gmail.com>
2026-08-12 01:02:07 +0530

    Changes errors

---
+++ src/customers/CustomerCreditNotes.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+function CustomerCreditNotes() {
+  return (
+    <div>
+      <h1>Customer Credit Notes are here</h1>
+    </div>
+  )
+}
+
+export default CustomerCreditNotes
