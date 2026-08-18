f7bb6d615c7851bf405035403a2fbee5ace0b25c
NishanDM <nishandm97@gmail.com>
2026-08-18 11:35:17 +0530

    Added Supplier Components

---
+++ src/suppliers/SupplierOutstandings.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function SupplierOutstandings() {
+  return (
+    <div>
+      <h1>
+        Supplier Outstandings
+      </h1>
+    </div>
+  )
+}
+
+export default SupplierOutstandings
